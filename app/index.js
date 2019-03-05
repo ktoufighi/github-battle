2144511ada9114d4f2b23686f738ac287075cc1f
Kathy Toufighi <kathy.toufighi@gmail.com>
2019-03-05 14:30:33 -0500

    validating components with propTypes

--- app/index.js
+++ app/index.js
@@ -1,20 +1,36 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-require('./index.css');
+var propTypes = require('prop-types');
 
-class App extends React.Component {
+class Badge extends React.Component {
   render() {
     return (
       <div>
-        Hello World!
+        <img
+          src={this.props.img}
+          alt="Avatar"
+          style={{width: 100, height: 100}}
+          />
+          <h1>Name: {this.props.name}</h1>
+          <h3>usename: {this.props.username}</h3>
       </div>
     )
   }
 }
 
+Badge.propTypes = {
+  img: propTypes.string.isRequired,
+  name: propTypes.string.isRequired,
+  username: propTypes.string.isRequired
+}
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+  <Badge
+    name= 'Kathy Toufighi'
+    username= 'ktoufighi'
+    img={'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'}
+  />,
+  document.getElementById('root')
 
-)
+);
